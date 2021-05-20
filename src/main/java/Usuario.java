2c862fd19c2f79e5f3d80b0e2e8d6fab25367744
Raul Martin Catalan <rmartinc96@gmail.com>
2021-05-20 19:48:54 +0200

    Clase Usuario creada

---
+++ src/main/java/Usuario.java
@@ -0,0 +1,38 @@
+public class Usuario {
+    private String usuario;
+    private String apellido;
+    private String email;
+    private String contraseña;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
